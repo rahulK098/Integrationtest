e54da0187ca52e09e963a3d2bd5c70dfec7edb50
shicvx <shivramas3556@gmail.com>
2023-05-19 14:55:32 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Integrationtest
+
+Integration testing on user Service of an Open source sock shop Project.
+Using jest and supertest.
+
+## Run 
+```
+docker-compose up
+```
+
+## Result
+✓ Get Login user
+
+✓ Get List of Customers
---
+++ tests/index.test.js
@@ -0,0 +1,52 @@
+const supertest = require('supertest');
+const isDog = require('../index.js')
+
+// test
+describe('Is dog function', () => {
+  it('Is dog', () => {
+    expect(isDog("Dog")).toBe(true);
+  });
+})
+
+// User Service Tests
+describe('user Service TEST Suites', () => {
+  const request = supertest('http://user-test:80');
+
+
+  beforeAll(async () => {
+  });
+
+  afterAll(async () => {
+  });
+
+  beforeEach(() => {
+
+  });
+
+
+  it('Get WrongUrl /user/login', async () => {
+    const response = await
+      request.get("/user")
+
+    expect(response.status).toBe(404);
+  });
+
+  it('Get Login user', async () => {
+    const response = await
+      request.get("/login")
+        .auth('user', 'password', { type: "basic" })
+
+    expect(response.status).toBe(200);
+  });
+
+
+  it('Get List of Customers', async () => {
+    const response = await
+      request.get("/customers")
+
+    expect(response.status).toBe(200);
+  });
+
+  
+
+});
